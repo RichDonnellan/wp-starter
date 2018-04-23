5023e30ddd766696b03c15110384fd2ab1083873
Rich Donnellan <rdonnellan@contactaac.com>
2018-04-23 12:48:55 -0700

    Update footer styling

--- lib/structure/footer.view.php
+++ lib/structure/footer.view.php
@@ -1,14 +1,10 @@
 <footer role="contentinfo">
     <?= render('/lib/components/cta/footer/cta-footer.php', ['content' => 'Recovery is Worth Fighting For. We’ll Help You.']); ?>
     <div class="bg-primary border-t border-primary-400">
-        <div class="flex container justify-center py-4 text-white text-sm">
-            <p class="flex flex-col justify-center items-center md:flex-row">
-                <span class="">&copy; <?= THEME_NAME ?> <?= date('Y'); ?> All Rights Reserved.</span>
-
-                <?= render('/lib/components/navigation/nav-footer.php'); ?>
-
-                <span class=""><?= THEME_NAME ?> is a property of Sober Media Group.</span>
-            </p>
+        <div class="container flex flex-col justify-center items-center py-4 text-white text-sm md:flex-row">
+            <p>&copy; <?= THEME_NAME ?> <?= date('Y'); ?> &bullet; All Rights Reserved.</p>
+            <?= render('/lib/components/navigation/nav-footer.php'); ?>
+            <p><?= THEME_NAME ?> is a property of Sober Media Group.</p>
         </div>
     </div>
 </footer>
