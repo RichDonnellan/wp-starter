77b9f3eb7e020918ef0b32160c1dd71deb8ec03b
Rich Donnellan <rdonnellan@contactaac.com>
2018-04-23 14:02:59 -0700

    Add tab support

--- lib/components/compliance/compliance.php
+++ lib/components/compliance/compliance.php
@@ -1,6 +1,6 @@
 <?php if (!isset($data)) return; ?>
 
-<div class="flex items-center <?= exists($data->modifier) ?>">
+<div class="flex items-center <?= exists($data->modifier) ?>" tabindex="0">
     <span class="inline-block mr-1"><?= render('/lib/components/compliance/icon-info.svg', ['modifier' => 'text-secondary']); ?></span>
     <span><?= exists($data->content) ?></span>
 </div>
