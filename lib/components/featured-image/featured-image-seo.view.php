644d65174b7ef376c955122b9012aacb674eeedb
Rich Donnellan <rdonnellan@contactaac.com>
2018-05-09 13:28:51 -0700

    Add bottom spacing to featured image

--- lib/components/featured-image/featured-image-seo.view.php
+++ lib/components/featured-image/featured-image-seo.view.php
@@ -1,4 +1,4 @@
 <div
     style="background-image: url(<?= $featured_image->image ?>)"
-    class="featured-image--<?= $featured_image->size ?> bg-primary-200 container--no-center">
+    class="featured-image--<?= $featured_image->size ?> bg-primary-200 container--no-center mb-8">
 </div>
