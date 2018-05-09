da04311fc54a80e009a9c6e32a39e6b84fd42924
Rich Donnellan <rdonnellan@contactaac.com>
2018-05-09 13:25:24 -0700

    Remove title and description meta

--- lib/partials/head.php
+++ lib/partials/head.php
@@ -3,8 +3,6 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-        <meta name="description" content="<?php bloginfo('description'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="format-detection" content="telephone=no">
         <?php wp_head(); ?>
