6efeae6836b655b1ff5cff6131a2b4edfe933181
Rich Donnellan <rdonnellan@contactaac.com>
2018-05-23 16:16:50 -0700

    Use the correct function to get the post thumb URL

--- lib/functions/featured-image.php
+++ lib/functions/featured-image.php
@@ -1,11 +1,19 @@
 <?php
+/**
+ * Retrieves the post thumbnail or default_image-${size} URL
+ *
+ * @param string $size - Will either be full (default) or seo
+ * @param string $ext - Used for default featured image extension
+ * @return string
+ */
 
 function getFeaturedImage($size = 'full', $ext = 'jpg')
 {
+    global $post;
     $default_image = 'default-featured-image';
-    $fi = (object) [
-        'image' => get_the_post_thumbnail($size) ?: imagePath("$default_image-${size}.${ext}"),
+    $featured_image = (object) [
+        'image' => get_the_post_thumbnail_url($post->ID, "featured-${size}") ?: imagePath("$default_image-${size}.${ext}"),
         'size'  => $size,
     ];
-    return $fi;
+    return $featured_image;
 }
