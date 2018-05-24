4b1087d7b1630d5aae077bc19595d193384220a9
Rich Donnellan <rdonnellan@contactaac.com>
2018-05-24 14:32:43 -0700

    Add config and setup to proper hook

--- lib/setup.php
+++ lib/setup.php
@@ -1,45 +1,53 @@
 <?php
 
-$soil = [
-/**
- * Enable features from Soil when plugin is activated
- * @link https://roots.io/plugins/soil/
- */
-    'soil-clean-up' => null,
-    'soil-disable-asset-versioning' => null,
-    'soil-disable-trackbacks' => null,
-    'soil-jquery-cdn' => null,
-    'soil-js-to-footer' => null,
-    'soil-nav-walker' => null,
-    'soil-nice-search' => null,
-    'soil-relative-urls' => null,
-];
+add_action('after_setup_theme', 'setupTheme');
+function setupTheme()
+{
+    global $registered_image_sizes; // Set to global so the filter function can access
 
-$theme_support = [
-    'title-tag' => null,
-    'post-thumbnails' => null,
-    'yoast-seo-breadcrumbs' => null,
-];
+    $soil = [
+        /**
+         * Enable features from Soil when plugin is activated
+         * @link https://roots.io/plugins/soil/
+         */
+        'soil-clean-up' => null,
+        'soil-disable-asset-versioning' => null,
+        'soil-disable-trackbacks' => null,
+        'soil-jquery-cdn' => null,
+        'soil-js-to-footer' => null,
+        'soil-nav-walker' => null,
+        'soil-nice-search' => null,
+        'soil-relative-urls' => null,
+    ];
 
-$menus = [
-    'primary' => __('Primary Navigation', TEXT_DOMAIN),
-    'primary-mobile' => __('Mobile Navigation', TEXT_DOMAIN),
-    'footer' => __('Footer Navigation', TEXT_DOMAIN),
-];
+    $theme_support = [
+        'title-tag' => null,
+        'post-thumbnails' => null,
+        'yoast-seo-breadcrumbs' => null,
+    ];
 
-$image_sizes = [
-    'featured-seo' => [
-        'width' => 760,
-        'height' => 265,
-        'crop' => true
-    ],
-    'featured-full' => [
-        'width' => 1440,
-        'height' => 265
-    ]
-];
+    $menus = [
+        'primary' => __('Primary Navigation', TEXT_DOMAIN),
+        'primary-mobile' => __('Mobile Navigation', TEXT_DOMAIN),
+        'footer' => __('Footer Navigation', TEXT_DOMAIN),
+    ];
 
-registerThemeSupport($soil);
-registerThemeSupport($theme_support);
-registerNavMenus($menus);
-registerImageSizes($image_sizes);
+    $registered_image_sizes = [
+        'featured-seo' => [
+            'width' => 760,
+            'height' => 265,
+            'crop' => true,
+            'displayed_as' => __('Featured SEO Image', TEXT_DOMAIN),
+        ],
+        'featured-full' => [
+            'width' => 1440,
+            'height' => 265,
+            'displayed_as' => __('Featured Full-Width Image', TEXT_DOMAIN),
+        ],
+    ];
+
+    registerThemeSupport($soil);
+    registerThemeSupport($theme_support);
+    registerNavMenus($menus);
+    registerImageSizes($registered_image_sizes);
+}
