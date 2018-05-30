0c9b108cc700f1dca100843db0be98272bece3b9
Rich Donnellan <rdonnellan@contactaac.com>
2018-05-30 10:38:48 -0700

    Wrap modal data objects in exists function

--- lib/components/modal/modal.php
+++ lib/components/modal/modal.php
@@ -1,14 +1,14 @@
-  <div class="modal micromodal-slide" id="<?= $data->id ?>" aria-hidden="true">
+  <div class="modal micromodal-slide" id="<?= exists($data->id) ?>" aria-hidden="true">
     <div class="modal__overlay" tabindex="-1" data-micromodal-close>
-      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="<?= $data->id ?>-title">
+      <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="<?= exists($data->id) ?>-title">
         <header class="modal__header">
-          <h2 class="modal__title" id="<?= $data->id ?>-title">
-            <?= $data->title ?>
+          <h2 class="modal__title" id="<?= exists($data->id) ?>-title">
+            <?= exists($data->title) ?>
           </h2>
           <button class="modal__close" aria-label="Close modal" data-micromodal-close>&times;</button>
         </header>
-        <main class="modal__content" id="<?= $data->id ?>-content">
-            <?= $data->content ?>
+        <main class="modal__content" id="<?= exists($data->id) ?>-content">
+            <?= exists($data->content) ?>
         </main>
       </div>
     </div>
