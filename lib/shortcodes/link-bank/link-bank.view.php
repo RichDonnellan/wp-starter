4182d7b3ce3452485adcdba0e0807038e57ebbe3
Rich Donnellan <rdonnellan@contactaac.com>
2018-07-16 12:11:51 -0700

    Add link bank shortcode

---
+++ lib/shortcodes/link-bank/link-bank.view.php
@@ -0,0 +1,4 @@
+<div class="<?= $shortcode_name ?> <?= $modifier ?>">
+    <div class="<?= $shortcode_name ?>__title"><?= $title ?></div>
+    <div class="<?= $shortcode_name ?>__content"><?= $the_content ?></div>
+</div>
