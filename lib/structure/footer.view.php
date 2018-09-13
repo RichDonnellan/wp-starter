01cef31e204c35026849ebef2a9607f8b5d633e9
Rich Donnellan <rdonnellan@contactaac.com>
2018-09-13 12:46:37 -0700

    Fix styling of footer and update author global

--- lib/structure/footer.view.php
+++ lib/structure/footer.view.php
@@ -4,7 +4,7 @@
         <div class="container flex flex-col justify-center items-center py-4 text-white text-sm md:flex-row">
             <p>&copy; <?= THEME_NAME ?> <?= date('Y'); ?> &bullet; All Rights Reserved.</p>
             <?= render('/lib/components/navigation/nav-footer.php'); ?>
-            <p><?= THEME_NAME ?> is operated by Recovery Brands LLC, a subsidiary of American Addiction Centers, Inc.</p>
+            <p class="text-center mt-4 md:text-right md:mt-0"><?= THEME_NAME ?> is operated by Recovery Brands LLC, a subsidiary of <?= CORP_NAME ?></p>
         </div>
     </div>
 </footer>
