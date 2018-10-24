f2a842cc8850494b60f3eea73e40594691ec2f7b
Rich Donnellan <rdonnellan@contactaac.com>
2018-10-24 15:51:22 -0700

    Update Google Tag Manager script
    - Add GTM_ID constant
    - Update/add partial(s)
    - Inject to correct location (head/body)

--- lib/partials/head.php
+++ lib/partials/head.php
@@ -5,6 +5,11 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="format-detection" content="telephone=no">
-        <?php wp_head(); ?>
+        <?php
+            wp_head();
+            include(THEME_DIR . '/lib/partials/google-tag-manager.php');
+        ?>
+
     </head>
     <body <?php body_class('font-sans leading-normal text-dark'); ?>>
+    <?php include(THEME_DIR . '/lib/partials/ns-google-tag-manager.php'); ?>
