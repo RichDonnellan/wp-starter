d4f8ea177887ed0c933eac799d3cbcbf20a09c55
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-20 14:31:57 -0800

    Reorganize cta components

--- footer.php
+++ footer.php
@@ -3,14 +3,14 @@
 render('/lib/structure/footer-partnership.view.php');
 
 if(!isPostType('content-marketing')) {
-    $cta_footer = render('/lib/components/cta/footer/cta-footer.php', ['content' => 'Recovery is Worth Fighting For. We\'ll Help You.']);
+    $cta_footer = render('/lib/components/cta/cta-footer.php', ['content' => 'Recovery is Worth Fighting For. We\'ll Help You.']);
 }
 
 render('/lib/structure/footer.view.php');
 
 if (!isPosttype('content-marketing')) {
 
-    render('/lib/components/cta/sticky/cta-sticky-mobile.php', ['content' => 'Find a Top Alcoholic Rehab Center Today.']);
+    render('/lib/components/cta/cta-sticky-mobile.php', ['content' => 'Find a Top Alcoholic Rehab Center Today.']);
     render('/lib/components/modal/modal.php', ['id' => 'modal-helpline-disclaimer', 'title' => 'How Our Helpline Works', 'content' => '<p>For those seeking addiction treatment for themselves or a loved one, the Alcohol.org helpline is a private and convenient solution.</p>
     <p><strong>Calls to any general helpline (non-facility specific 1-8XX numbers) for your visit will be answered by American Addiction Centers (AAC).</strong></p>
     <p>We are standing by 24/7 to discuss your treatment options. Our representatives work solely for AAC and will discuss whether an AAC facility may be an option for you.</p>
