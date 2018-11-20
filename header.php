3d4adf8cfcfce36583d3e8697b7bc4769d4c2a69
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-20 14:18:19 -0800

    Remove header cta-sticky

--- header.php
+++ header.php
@@ -1,4 +1,3 @@
 <?php
 render('/lib/partials/head.php');
-render('/lib/components/cta/sticky/cta-sticky.php', ['content' => 'Find a Top Alcoholic Rehab Center Today.']);
 render('/lib/structure/header.view.php');
