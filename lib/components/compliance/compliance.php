64385f447327bc52fd2a61d5c59b28bdf43c3170
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-20 11:32:00 -0800

    Use lighter secondary color for CTA button

--- lib/components/compliance/compliance.php
+++ lib/components/compliance/compliance.php
@@ -1,6 +1,6 @@
 <?php if (!isset($data)) return; ?>
 
 <button role="button" class="cursor-pointer <?= exists($data->modifier) ?>" tabindex="0" data-micromodal-trigger="modal-helpline-disclaimer">
-    <span class="inline-block mr-1"><?= render('/lib/components/compliance/icon-info.svg', ['modifier' => 'text-secondary']); ?></span>
+    <span class="inline-block mr-1"><?= render('/lib/components/compliance/icon-info.svg', ['modifier' => 'text-secondary-100']); ?></span>
     <span><?= exists($data->content) ?></span>
 </button>
