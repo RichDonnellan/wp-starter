d692eb65adac93dbc402570b252a74c4295e95a5
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-21 14:17:47 -0800

    Update hero

--- lib/structure/front-page/hero.view.php
+++ lib/structure/front-page/hero.view.php
@@ -1,12 +1,14 @@
-<div class="hero overlay--tinted">
-    <div class="flex flex-col flex-center container relative z-5 text-white text-center">
-        <h1 class="<?= exists($data->modifier) ?>">
-            <?= exists($data->title) ?>
-        </h1>
-        <p class="<?= exists($data->modifier) ?>">
-            <?= exists($data->subtitle) ?>
-        </p>
-        <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call '.CALLRAIL_PHONE, 'modifier' => 'button--large mt-4']); ?>
+<div class="hero">
+    <div class="flex flex-col flex-center container text-white text-center max-w-xl">
+        <div class="-mt-8 md:-mt-6">
+            <h1 class="leading-tight md:leading-normal font-bold mb-4 text-shadow--base">
+                <?= exists($data->title) ?>
+            </h1>
+            <p class="font-serif md:leading-loose text-shadow--base">
+                <?= exists($data->subtitle) ?>
+            </p>
+        </div>
+        <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call '.CALLRAIL_PHONE, 'modifier' => 'md:text-xl my-4']); ?>
         <?= render('/lib/components/compliance/compliance.php'); ?>
     </div>
 </div>
