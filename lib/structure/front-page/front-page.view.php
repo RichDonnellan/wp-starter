d638299386b9c88ebf3c6129d152f60fdd079827
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-21 14:43:25 -0800

    Add bg color to main content area

--- lib/structure/front-page/front-page.view.php
+++ lib/structure/front-page/front-page.view.php
@@ -1,6 +1,7 @@
 <?php render('/lib/structure/front-page/hero.view.php', $data->hero); ?>
 
-<main id="primary-content" role="main">
+<main id="primary-content" role="main" class="bg-light-100">
+    <?php render('/lib/structure/front-page/stats.view.php', $data->stats); ?>
     <?php render('/lib/structure/front-page/mission.view.php', $data->mission); ?>
     <?php render('/lib/structure/front-page/topics.view.php', $data->topics); ?>
 </main>
