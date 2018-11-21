9fe4838fcfeea0989bc05d8569f948b380cb1e63
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-21 14:04:31 -0800

    Update footer

--- lib/components/cta/cta-footer.php
+++ lib/components/cta/cta-footer.php
@@ -1,9 +1,7 @@
-<div class="cta--footer flex text-center overlay--base">
-    <div class="flex flex-col container justify-center items-center">
-        <div class="relative text-white h2 <?= exists($data->modifier) ?>">
-            <?= exists($data->content) ?>
-            <div>Call <?= CALLRAIL_PHONE ?></div>
-            <?= render('/lib/components/compliance/compliance.php'); ?>
-        </div>
+<div class="cta--footer flex text-center">
+    <div class="flex flex-col flex-center container text-white">
+        <p class="h2 md:h1 font-bold text-shadow--base cta--has-arrow:dl "><?= exists($data->content) ?></p>
+        <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call '.CALLRAIL_PHONE, 'modifier' => 'md:text-xl my-4']); ?>
+        <?= render('/lib/components/compliance/compliance.php'); ?>
     </div>
 </div>
