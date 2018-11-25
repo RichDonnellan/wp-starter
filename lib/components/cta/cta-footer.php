5f21d57e7b3be39e559489014641fe77269233bb
Rich Donnellan <rich.donnellan@gmail.com>
2018-11-25 15:45:12 -0800

    Position handwritten text and arrows
    Not the most elegant solution, but will have to revisit after launch.
    ¯\_(ツ)_/¯

--- lib/components/cta/cta-footer.php
+++ lib/components/cta/cta-footer.php
@@ -1,6 +1,6 @@
 <div class="cta--footer flex text-center">
     <div class="flex flex-col flex-center container text-white">
-        <p class="h2 md:h1 font-bold text-shadow--base cta--has-arrow:dl "><?= exists($data->content) ?></p>
+        <p class="h2 md:h1 font-bold text-shadow--base cta--has-arrow:dl relative"><?= exists($data->content) ?></p>
         <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call '.CALLRAIL_PHONE, 'modifier' => 'md:text-xl my-4']); ?>
         <?= render('/lib/components/compliance/compliance.php'); ?>
     </div>
--- lib/structure/header.view.php
+++ lib/structure/header.view.php
@@ -1,5 +1,5 @@
 <header class="bg-primary border-b border-primary-400" role="banner">
-    <div class="flex container justify-between items-stretch">
+    <div class="flex container justify-between items-stretch relative">
         <a class="py-2 self-center" href="<?= esc_url(home_url('/')) ?>" title="Back to <?php bloginfo('name') ?>"><?= render('/lib/partials/logo-tagline.svg'); ?></a>
         <?= render('/lib/components/navigation/nav-mobile.php'); ?>
         <?= render('/lib/components/navigation/nav-primary.php'); ?>
