6999cea64c5c2cb55fa9c12cf5df26909899013a
Rich Donnellan <rich.donnellan@gmail.com>
2018-11-25 17:14:30 -0800

    Fix incorrect font assignment

--- lib/structure/front-page/stats.view.php
+++ lib/structure/front-page/stats.view.php
@@ -2,9 +2,9 @@
 
 <section class="stats md:flex container -mt-24">
     <?php foreach($data as $stats) : ?>
-        <div class="leading-loose md:mb-0 flex-1 px-7 py-9 bg-white">
-            <h2 class="font-serif text-primary-400 font-bold h1 leading-none mb-7"><?= $stats->stat ?></h2>
-            <p class="font-sans font-bold text-sm"><?= $stats->content ?></p>
+        <div class="font-serif leading-loose md:mb-0 flex-1 px-7 py-9 bg-white">
+            <h2 class="text-primary-400 font-bold h1 leading-none mb-7"><?= $stats->stat ?></h2>
+            <p class="font-bold text-sm"><?= $stats->content ?></p>
         </div>
     <?php endforeach; ?>
 </section>
