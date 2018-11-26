4bca1aa051d773bf891960f52433c6e0589ea57c
Rich Donnellan <rich.donnellan@gmail.com>
2018-11-25 16:59:21 -0800

    Update footer partnership

--- lib/structure/footer-partnership.view.php
+++ lib/structure/footer-partnership.view.php
@@ -1,13 +1,13 @@
-<div class="bg-primary py-5">
-    <div class="container flex flex-col justify-center items-center md:flex-row">
-        <div class="mb-5">
-            <?= render('/lib/partials/logo.svg'); ?>
+<div class="container font-serif text-white text-center">
+    <div style="background: linear-gradient(270deg, #5e87a9 0%, #4b7596 100%);" class="flex flex-col mx-auto rounded-lg md:rounded m-9 p-6 md:p-9 max-w-xl">
+        <div class="flex flex-col flex-center md:flex-row mb-5">
+            <div class="mb-5 md:mb-0 md:pr-5 md:border-r md:border-transparent-white-80">
+                <?= render('/lib/partials/logo.svg'); ?>
+            </div>
+            <div class="md:ml-5">
+                <?= render('/lib/partials/logo-aac.svg'); ?>
+            </div>
         </div>
-        <div class="mb-5 md:ml-5">
-            <?= render('/lib/partials/logo-aac.svg'); ?>
-        </div>
-    </div>
-    <div class="container text-white text-center">
-        <p class="text-sm"><?= THEME_NAME ?> is an <?= CORP_NAME ?> subsidiary. Not sure what this means?  Learn more <a href="/about/" class="text-white hover:text-white">about AAC.</a></p>
+        <p><?= THEME_NAME ?> is an <?= CORP_NAME ?> subsidiary. Not sure what this means?  <a href="/about/" class="font-bold text-white hover:text-white">Learn more about AAC.</a></p>
     </div>
 </div>
