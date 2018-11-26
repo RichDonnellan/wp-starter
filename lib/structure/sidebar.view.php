67c25734183e8e1c900352a54dfe4ba1a4f90697
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-26 15:09:01 -0800

    Remove sidebar CTA

--- lib/structure/sidebar.view.php
+++ lib/structure/sidebar.view.php
@@ -1,4 +1,3 @@
 <aside role="complementary" class="w-full md:w-1/3 md:ml-10">
-    <?php include(THEME_DIR . '/lib/components/cta/cta-sidebar.php'); ?>
     <?php include(THEME_DIR . '/lib/components/further-reading/further-reading.php'); ?>
 </aside>
