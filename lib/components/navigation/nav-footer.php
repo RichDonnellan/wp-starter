156c0dc603f3bb004bab0c3f47a687fb3b749475
Rich Donnellan <rich.donnellan@gmail.com>
2018-11-25 16:11:13 -0800

    Update footer link styling

--- lib/components/navigation/nav-footer.php
+++ lib/components/navigation/nav-footer.php
@@ -1,7 +1,8 @@
 <?php
 
 $args = [
-    'theme_location' => 'footer'
+    'theme_location' => 'footer',
+    'menu_class' => 'font-bold'
 ];
 
 include(THEME_DIR . '/lib/components/navigation/nav-footer.view.php');
--- lib/structure/footer.view.php
+++ lib/structure/footer.view.php
@@ -1,7 +1,7 @@
 <footer role="contentinfo" data-js="offset-cta-sticky-mobile">
     <?= exists($cta_footer) ?>
     <div class="bg-primary border-t border-primary-400">
-        <div class="container flex flex-col justify-center items-center py-4 font-serif font-bold text-white text-sm md:flex-row">
+        <div class="container flex flex-col justify-center items-center py-4 font-serif text-white text-sm md:flex-row">
             <p>&copy; <?= THEME_NAME ?> <?= date('Y'); ?> &bullet; All Rights Reserved.</p>
             <?= render('/lib/components/navigation/nav-footer.php'); ?>
         </div>
