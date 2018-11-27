7b9041b1eb70c6ea1c8fd39762ea0b48a5734a1b
Rich Donnellan <rich.donnellan@gmail.com>
2018-11-26 22:43:09 -0800

    Adjust hero text widths

--- lib/structure/front-page/hero.view.php
+++ lib/structure/front-page/hero.view.php
@@ -1,10 +1,10 @@
 <div class="hero">
     <div class="flex flex-col flex-center container text-white text-center max-w-xl">
-        <div class="-mt-8 md:-mt-6">
-            <h1 class="leading-tight md:leading-normal font-bold mb-4 text-shadow--base">
+        <div class="flex flex-col -mt-8 md:-mt-6">
+            <h1 class="self-center max-w-md leading-tight md:leading-normal font-bold mb-4 text-shadow--base">
                 <?= exists($data->title) ?>
             </h1>
-            <p class="font-serif md:leading-loose text-shadow--base">
+            <p class="max-w-lg font-serif md:leading-loose text-shadow--base">
                 <?= exists($data->subtitle) ?>
             </p>
         </div>
