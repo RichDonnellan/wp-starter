25ca4c0278de1714fdf888380216a8ac2b7dc228
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-27 14:56:29 -0800

    Remove side margins which caused Safari to choke hard

--- lib/components/cta/cta-header.php
+++ lib/components/cta/cta-header.php
@@ -1,6 +1,6 @@
 <div class="hidden invisible lg:visible lg:block">
     <div class="flex flex-col justify-center items-center py-4">
-        <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call ' . CALLRAIL_PHONE, 'modifier' => 'mx-3 mb-3']); ?>
+        <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call ' . CALLRAIL_PHONE, 'modifier' => 'mb-3']); ?>
         <?= render('/lib/components/compliance/compliance.php'); ?>
     </div>
     <div style="right:1rem;bottom:-3rem;" class="font-handwritten text-white text-xl absolute z-5 cta--has-arrow:ul">Call American Addiction Centers to find help with alcoholic rehab.</div>
