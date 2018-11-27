d10e0be92d980e7bf304e4978915eb4c19ab3341
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-27 11:02:33 -0800

    Add z-index to comic sans text

--- lib/components/cta/cta-header.php
+++ lib/components/cta/cta-header.php
@@ -3,5 +3,5 @@
         <?= render('/lib/components/buttons/button-phone.php', ['phone' => CALLRAIL_PHONE, 'content' => 'Call ' . CALLRAIL_PHONE, 'modifier' => 'mx-3 mb-3']); ?>
         <?= render('/lib/components/compliance/compliance.php'); ?>
     </div>
-    <div style="right:1rem;bottom:-3rem;" class="font-handwritten text-white text-xl absolute cta--has-arrow:ul">Call American Addiction Centers to find help with alcoholic rehab.</div>
+    <div style="right:1rem;bottom:-3rem;" class="font-handwritten text-white text-xl absolute z-5 cta--has-arrow:ul">Call American Addiction Centers to find help with alcoholic rehab.</div>
 </div>
