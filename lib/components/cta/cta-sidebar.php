95e0c3aa6d52060de51c866449972a8a59601135
Rich Donnellan <rdonnellan@contactaac.com>
2018-11-27 13:50:14 -0800

    Update further reading block

--- lib/components/cta/cta-sidebar.php
+++ lib/components/cta/cta-sidebar.php
@@ -1,4 +1,4 @@
-<div class="cta--sidebar flex mb-8">
+<div class="cta--sidebar mb-8 hidden invisible md:flex md:visible">
     <div class="flex flex-col container justify-center items-center py-4">
         <div class="text-white text-center">
             <p class="text-xl font-bold mb-2">Ready to Get Clean & Sober?</p>
--- lib/components/further-reading/further-reading.view.php
+++ lib/components/further-reading/further-reading.view.php
@@ -1,12 +1,14 @@
 <?php if ($articles) { ?>
-<h2 class="font-bold mb-5">Further Reading</h2>
-    <ul class="list-reset">
+<div class="p-8 mb-6 bg-aqua-haze">
+    <h2 class="font-bold mb-5">Further Reading</h2>
+    <ul class="list-reset font-serif">
         <?php foreach($articles as $article) {
             $permalink = esc_url(get_permalink($article->ID));
         ?>
-            <li class="border-t"><a class="block py-6 pl-4 no-underline hover:underline" href="<?= $permalink; ?>" title="<?= $article->post_title; ?>">
+            <li class="border-t"><a class="font-bold inline-block py-6 no-underline hover:underline" href="<?= $permalink; ?>" title="<?= $article->post_title; ?>">
                 <?= exists($article->alt_title, $article->post_title) ?></a>
             </li>
         <?php } ?>
     </ul>
+</div>
 <?php } ?>
